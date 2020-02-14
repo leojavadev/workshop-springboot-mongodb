f4a6b5e5535fd2926a57daab101f884700804c48
leojavadev <leojavadev@gmail.com>
2020-02-14 11:51:20 -0300

    Implementação do Repository e Service, e da conexão com o MongoDB

--- src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
@@ -4,28 +4,39 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fling.workshopmongo.domain.User;
+import br.com.fling.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	//ou
 	//@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria", "maria@fling.com");
-		User joao = new User("2", "João", "joao@fling.com");
-		User erica = new User("3", "Érica", "erica@fling.com.br");
-		List<User> users = new ArrayList<>();
-		users.addAll(Arrays.asList(maria, joao, erica));
-		return ResponseEntity.ok().body(users);
+		
+		/*
+		 * User maria = new User("1", "Maria", "maria@fling.com"); User joao = new
+		 * User("2", "João", "joao@fling.com"); 
+		 * User erica = new User("3", "Érica",
+		 * "erica@fling.com.br"); 
+		 * List<User> users = new ArrayList<>();
+		 * users.addAll(Arrays.asList(maria, joao, erica));
+		 * return ResponseEntity.ok().body(users);
+		 */
+		
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 
 }
---
+++ src/main/java/br/com/fling/workshopmongo/services/UserService.java
@@ -0,0 +1,21 @@
+package br.com.fling.workshopmongo.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fling.workshopmongo.domain.User;
+import br.com.fling.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
