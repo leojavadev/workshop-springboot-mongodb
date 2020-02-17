2c2a1b2552078cc0336a4c87e4fab92619bf354b
leojavadev <leojavadev@gmail.com>
2020-02-17 15:19:27 -0300

    Buscar usuário por ID e tratamento de exceção da consulta

--- src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
@@ -1,10 +1,12 @@
 package br.com.fling.workshopmongo.resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,13 +36,16 @@ public class UserResource {
 		 * List<User> users = new ArrayList<>();
 		 * users.addAll(Arrays.asList(maria, joao, erica));
 		 * return ResponseEntity.ok().body(users);
-		 */
-		
+		 */	
 		
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);		 
-		
+		return ResponseEntity.ok().body(listDto);	
 	}
-
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){		
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
--- src/main/java/br/com/fling/workshopmongo/services/UserService.java
+++ src/main/java/br/com/fling/workshopmongo/services/UserService.java
@@ -1,13 +1,14 @@
 package br.com.fling.workshopmongo.services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.fling.workshopmongo.domain.User;
 import br.com.fling.workshopmongo.repository.UserRepository;
+import br.com.fling.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 }
