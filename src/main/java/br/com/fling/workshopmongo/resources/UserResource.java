febbe8535d0f5a06898dbe70c531e68d46793f29
leojavadev <leojavadev@gmail.com>
2020-02-18 11:24:06 -0300

    Implementação do método DELETE da entidade User

--- src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
@@ -58,4 +58,10 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);
+		return ResponseEntity.noContent().build(); //código http 204 é noContent()
+	}
 }
