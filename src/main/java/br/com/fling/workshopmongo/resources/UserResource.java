5019a601c84b52ed6e7930e29fc6ca7774fb6176
leojavadev <leojavadev@gmail.com>
2020-02-17 22:48:52 -0300

    Implementação do método insert com POST

--- src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
+++ src/main/java/br/com/fling/workshopmongo/resources/UserResource.java
@@ -1,15 +1,17 @@
 package br.com.fling.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.fling.workshopmongo.domain.User;
 import br.com.fling.workshopmongo.dto.UserDTO;
@@ -43,9 +45,17 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);	
 	}
 	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id){		
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@RequestMapping(method = RequestMethod.POST) //ou @PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
